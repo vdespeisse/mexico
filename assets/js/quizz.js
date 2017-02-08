a29dea654f9d54291b0a9d060cc9ab7a37a9dfbb
vdespeisse <v.despeisse@gmail.com>
2017-02-08 18:45:10 +0100

    first try

--- assets/js/quizz.js
+++ assets/js/quizz.js
@@ -105,19 +105,70 @@ empreinte = [
             ]
           },
           {
-            text: "Quelle est la saisonnalité des vols XL Airways à destination du Mexique ?",
+            text: "Pour un séjour à Mexico, Empreinte propose un très bon hôtel au rapport qualité prix imbattable et proche du Zocalo, quel est-il ?",
             responses: [
-              {text: 'Ils opèrent uniquement en hiver'},
-              {text: 'Ils opèrent uniquement en été'},
-              {text: 'Ils opèrent toute l’année', correct: true},
+              {text: 'Mision San Miguel'},
+              {text: 'Quinta las Acacias'},
+              {text: 'Hamptonn Inn', correct: true},
             ]
           },
           {
-            text: "Quelle est la nationalité de la compagnie XL Airways ?",
+            text: "Comment s’appelle le programme d’excursions qu’Empreinte propose à ses clients sur la Riviera Maya par l’intermédiaire de son réceptif exclusif proposant la nage avec les dauphins, la visite de Chichen Itza, d’un cenote et des sites archéologiques de Coba et Tulum ?",
             responses: [
-              {text: 'C’est une compagnie américaine'},
-              {text: 'C’est un groupe anglais'},
-              {text: 'C’est une compagnie française', correct: true},
+              {text: 'Emotion Maya'},
+              {text: 'Passion Yucatan', correct: true},
+              {text: 'Vibration mexicaine'},
+            ]
+          },
+]
+
+mexique = [
+          {
+            text: "Concernant les nuits d’hôtels, quelle est la capacité d’accueil à la Riviera Maya ?",
+            responses: [
+              {text: '10,000 chambres '},
+              {text: '35,000 chambres'},
+              {text: '+40,000 chambres', correct: true},
+            ]
+          },
+          {
+            text: "Combien de vols atterrissent chaque mois à l’aéroport international de Cancun ?",
+            responses: [
+              {text: '4,500 vols mensuels', correct: true},
+              {text: '3,000 vols mensuels'},
+              {text: '500 vols mensuels'},
+            ]
+          },
+          {
+            text: "Quelle langue autre que l’espagnol a présence en la Riviera Maya à cause son caractère international ?",
+            responses: [
+              {text: 'Allemand'},
+              {text: 'Chinois'},
+              {text: 'Anglais', correct: true},
+            ]
+          },
+          {
+            text: "D'un point de vue géographique, le long la côte est plat à l'exception de quel site archéologique ?",
+            responses: [
+              {text: 'Tulum', correct: true},
+              {text: 'Akumal'},
+              {text: 'Coba'},
+            ]
+          },
+          {
+            text: "Qu’est ce qu'un cénote ?",
+            responses: [
+              {text: 'Une grotte replie d’eau au fil du temps', correct: true},
+              {text: 'Un plateau calcaire'},
+              {text: "Le nom d'une plage à Cancun"},
+            ]
+          },
+          {
+            text: "Quelle place occupe le Mexique dans le rating mondial des pays plus visités ?",
+            responses: [
+              {text: '11ème'},
+              {text: '9ème', correct: true},
+              {text: '3ème'},
             ]
           },
 ]
